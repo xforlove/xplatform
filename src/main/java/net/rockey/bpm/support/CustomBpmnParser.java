0a8ee2184e0b41a5b48b59c1acb536f4a710e49f
xforlove <rockey_du@163.com>
2014-08-24 20:58:32 +0800

    #V0.2.1

---
+++ src/main/java/net/rockey/bpm/support/CustomBpmnParser.java
@@ -0,0 +1,17 @@
+package net.rockey.bpm.support;
+
+import org.activiti.engine.impl.bpmn.parser.BpmnParser;
+import org.activiti.engine.impl.bpmn.parser.factory.ActivityBehaviorFactory;
+import org.activiti.engine.impl.bpmn.parser.factory.DefaultActivityBehaviorFactory;
+
+public class CustomBpmnParser extends BpmnParser {
+
+	// 当使用自定义activityBehaviorFactory 时，需要重写该方法
+	public void setActivityBehaviorFactory(
+			ActivityBehaviorFactory activityBehaviorFactory) {
+		((DefaultActivityBehaviorFactory) activityBehaviorFactory)
+				.setExpressionManager(expressionManager);
+		super.setActivityBehaviorFactory(activityBehaviorFactory);
+	}
+
+}
